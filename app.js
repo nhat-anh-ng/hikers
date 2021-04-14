989306eb38bd3c954d9c19fd6a79f5c240cc5da4
Nhat <nhatanh1nguyen@gmail.com>
2021-04-14 23:56:41 +0200

    edit , methodOvverride

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override"); //to fake a request
 const HikingPlace = require("./models/hikers");
 
 mongoose.connect("mongodb://localhost:27017/hikers", {
@@ -20,6 +21,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true })); // tell express to parse the body
+app.use(methodOverride("_method")); //query string used in edit.ejs
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -29,10 +33,29 @@ app.get("/hiking-places", async (req, res) => {
   res.render("hiking-places/index", { hikingPlaces }); //pass to template
 });
 
+app.get("/hiking-places/new", (req, res) => {
+  res.render("hiking-places/new");
+});
+
+app.post("/hiking-places", async (req, res) => {
+  const hikingPlace = new HikingPlace(req.body.hikingPlace);
+  await hikingPlace.save();
+  res.redirect(`/hiking-places/${hikingPlace._id}`);
+});
+
 app.get("/hiking-places/:id", async (req, res) => {
   const hikingPlace = await HikingPlace.findById(req.params.id);
   res.render("hiking-places/show", { hikingPlace }); //take id and look up corresponding place
 }); //id to look up corresponding hiking place from data
+
+app.get("/hiking-places/:id/edit", async (req, res) => {
+  const hikingPlace = await HikingPlace.findById(req.params.id);
+  res.render("hiking-places/edit", { hikingPlace });
+});
+
+app.put("/hiking-places/:id", async (req, res) => {
+  res.send("It worked!");
+});
 
 app.listen(3000, () => {
   console.log("port 3000 serving");

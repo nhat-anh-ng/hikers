14a963c72384697471008b734edc06441af3153f
Nhat <nhatanh1nguyen@gmail.com>
2021-04-18 16:31:26 +0200

    add img and description

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,14 @@ const seedDB = async () => {
   for (let i = 0; i < 3; i++) {
     //loop 50 places
     const random1000 = Math.floor(Math.random() * 3); //from random 1k
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new HikingPlace({
       //set location
       location: ` ${cities[random1000].city} ,${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://unsplash.com/photos/QRU0i5AqEJA",
+      description: "lorem ipsum",
+      price: price,
     });
     await camp.save();
   }
